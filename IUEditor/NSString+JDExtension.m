9a238f3f52d3d0546fe60eb9b2e6dc838cf4acc0
JD Yang <j@jdlab.org>
2014-07-14 15:04:08 +0900

    Add fallback code for HTMLID

--- IUEditor/NSString+JDExtension.m
+++ IUEditor/NSString+JDExtension.m
@@ -24,7 +24,7 @@
 }
 
 +(NSString*)randomStringWithLength:(int)len{
-    NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     NSMutableString *randomString = [NSMutableString stringWithCapacity: len];
     for (int i=0; i<len; i++) {
             [randomString appendFormat: @"%C", [letters characterAtIndex: arc4random_uniform((u_int32_t)[letters length]) % [letters length]]];
