ef5785e1193276339fc1fcfedc8d2ed8bb18ced0
jiwon <w@jdlab.org>
2014-07-15 15:19:32 +0900

    add wppagelinks

---
+++ IUEditor/WPPageLinks.h
@@ -0,0 +1,13 @@
+//
+//  WPPageLink.h
+//  IUEditor
+//
+//  Created by jw on 7/15/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import "IUPHP.h"
+
+@interface WPPageLinks : IUPHP
+
+@end
---
+++ IUEditor/WPPageLinks.m
@@ -0,0 +1,15 @@
+//
+//  WPPageLink.m
+//  IUEditor
+//
+//  Created by jw on 7/15/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import "WPPageLinks.h"
+
+@implementation WPPageLinks
+- (NSString*)code{
+    return @"global $wp_query; $big = 999999999;    echo paginate_links( array('base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),'format' => '?paged=%#%','current' => max( 1, get_query_var('paged') ),'total' => $wp_query->max_num_pages,  ) ); ";
+}
+@end
