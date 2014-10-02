9bed7c17e1c945b8a1bc38e139120751d249d289
SeungmiChoi <s@jdlab.org>
2014-10-02 17:18:20 +0900

    refactoring : frame

--- IUEditor/LMAppearanceFrameVC.m
+++ IUEditor/LMAppearanceFrameVC.m
@@ -52,6 +52,15 @@
 @property (weak) IBOutlet NSButton *wUnitBtn;
 @property (weak) IBOutlet NSButton *hUnitBtn;
 
+//nil button
+@property (weak) IBOutlet NSButton *xNilBtn;
+@property (weak) IBOutlet NSButton *yNilBtn;
+@property (weak) IBOutlet NSButton *wNilBtn;
+@property (weak) IBOutlet NSButton *hNilBtn;
+@property (weak) IBOutlet NSButton *minWNilBtn;
+@property (weak) IBOutlet NSButton *minHNilBtn;
+
+
 @property (weak) IBOutlet NSButton *centerBtn;
 @property (weak) IBOutlet NSButton *verticalCenterBtn;
 
@@ -88,18 +97,51 @@
     NSDictionary *percentHiddeBindingOption = [NSDictionary
                                             dictionaryWithObjects:@[[NSNumber numberWithBool:NO], NSNegateBooleanTransformerName]
                                             forKeys:@[NSRaisesForNotApplicableKeysBindingOption, NSValueTransformerNameBindingOption]];
+   
     
-    [self addObserverForCSSTag:IUCSSTagPixelX options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagPixelY options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagPixelWidth options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagPixelHeight options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagPercentX options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagPercentY options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagPercentWidth options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagPercentHeight options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagMinPixelHeight options:0 context:nil];
-    [self addObserverForCSSTag:IUCSSTagMinPixelWidth options:0 context:nil];
+    //x value
+    [self outlet:_xTF bind:NSValueBinding cssTag:IUCSSTagPixelX];
+    [self outlet:_xStepper bind:NSValueBinding cssTag:IUCSSTagPixelX];
+    [self outlet:_pxTF bind:NSValueBinding cssTag:IUCSSTagPercentX];
+    [self outlet:_pxStepper bind:NSValueBinding cssTag:IUCSSTagPercentX];
     
+    //y value
+    [self outlet:_yTF bind:NSValueBinding cssTag:IUCSSTagPixelY];
+    [self outlet:_yStepper bind:NSValueBinding cssTag:IUCSSTagPixelY];
+    [self outlet:_pyTF bind:NSValueBinding cssTag:IUCSSTagPercentY];
+    [self outlet:_pyStepper bind:NSValueBinding cssTag:IUCSSTagPercentY];
+
+    //width value
+    [self outlet:_wTF bind:NSValueBinding cssTag:IUCSSTagPixelWidth];
+    [self outlet:_wStepper bind:NSValueBinding cssTag:IUCSSTagPixelWidth];
+    [self outlet:_pwTF bind:NSValueBinding cssTag:IUCSSTagPixelWidth];
+    [self outlet:_pwStepper bind:NSValueBinding cssTag:IUCSSTagPixelWidth];
+    
+    //height value
+    [self outlet:_hTF bind:NSValueBinding cssTag:IUCSSTagPixelHeight];
+    [self outlet:_hStepper bind:NSValueBinding cssTag:IUCSSTagPixelHeight];
+    [self outlet:_phTF bind:NSValueBinding cssTag:IUCSSTagPercentHeight];
+    [self outlet:_phStepper bind:NSValueBinding cssTag:IUCSSTagPercentHeight];
+    
+    //min value
+    [self outlet:_minWStepper bind:NSValueBinding cssTag:IUCSSTagMinPixelWidth];
+    [self outlet:_minWTF bind:NSValueBinding cssTag:IUCSSTagMinPixelWidth];
+    [self outlet:_minHTF bind:NSValueBinding cssTag:IUCSSTagMinPixelHeight];
+    [self outlet:_minHStepper bind:NSValueBinding cssTag:IUCSSTagMinPixelHeight];
+
+    //unit value
+    [self outlet:_xUnitBtn bind:NSValueBinding cssTag:IUCSSTagXUnitIsPercent];
+    [self outlet:_yUnitBtn bind:NSValueBinding cssTag:IUCSSTagYUnitIsPercent];
+    [self outlet:_wUnitBtn bind:NSValueBinding cssTag:IUCSSTagWidthUnitIsPercent];
+    [self outlet:_hUnitBtn bind:NSValueBinding cssTag:IUCSSTagHeightUnitIsPercent];
+    
+    //other value
+    [self outlet:_positionPopupBtn bind:NSSelectedIndexBinding property:@"positionType"];
+    [self outlet:_centerBtn bind:NSValueBinding property:@"enableHCenter"];
+    [self outlet:_verticalCenterBtn bind:NSValueBinding property:@"enableVCenter"];
+    
+
+    //hidden
     [self outlet:_xTF bind:NSHiddenBinding cssTag:IUCSSTagXUnitIsPercent];
     [self outlet:_xStepper bind:NSHiddenBinding cssTag:IUCSSTagXUnitIsPercent];
     [self outlet:_pxTF bind:NSHiddenBinding cssTag:IUCSSTagXUnitIsPercent options:percentHiddeBindingOption];
@@ -128,15 +170,6 @@
 
     
     
-    [self outlet:_xUnitBtn bind:NSValueBinding cssTag:IUCSSTagXUnitIsPercent];
-    [self outlet:_yUnitBtn bind:NSValueBinding cssTag:IUCSSTagYUnitIsPercent];
-    [self outlet:_wUnitBtn bind:NSValueBinding cssTag:IUCSSTagWidthUnitIsPercent];
-    [self outlet:_hUnitBtn bind:NSValueBinding cssTag:IUCSSTagHeightUnitIsPercent];
-    
-    [self outlet:_positionPopupBtn bind:NSSelectedIndexBinding property:@"positionType"];
-    [self outlet:_centerBtn bind:NSValueBinding property:@"enableHCenter"];
-    [self outlet:_verticalCenterBtn bind:NSValueBinding property:@"enableVCenter"];
-
     //enabled option 1
     [self outlet:_xTF bind:NSEnabledBinding property:@"hasX"];
     [self outlet:_yTF bind:NSEnabledBinding property:@"hasY"];
@@ -208,8 +241,6 @@
     [self outlet:_minHTF bind:@"enabled2" property:@"canChangeHeightByUserInput"];
     [self outlet:_minHStepper bind:@"enabled2" property:@"canChangeHeightByUserInput"];
 
-    
-    
     [self outlet:_centerBtn bind:@"enabled2" property:@"xPosMove" options:IUBindingNegationAndNotRaise];
 
     
@@ -225,237 +256,38 @@
     
 }
 
+- (IBAction)clickNilBtn:(id)sender {
+    if([sender isEqualTo:_xNilBtn]){
+        [self setValue:nil forCSSTag:IUCSSTagPixelX];
+        [self setValue:nil forCSSTag:IUCSSTagPercentX];
+    }
+    else if([sender isEqualTo:_yNilBtn]){
+        [self setValue:nil forCSSTag:IUCSSTagPixelY];
+        [self setValue:nil forCSSTag:IUCSSTagPercentY];
+    }
+    else if([sender isEqualTo:_wNilBtn]){
+        [self setValue:nil forCSSTag:IUCSSTagPixelWidth];
+        [self setValue:nil forCSSTag:IUCSSTagPercentWidth];
+    }
+    else if([sender isEqualTo:_hNilBtn]){
+        [self setValue:nil forCSSTag:IUCSSTagPixelHeight];
+        [self setValue:nil forCSSTag:IUCSSTagPercentHeight];
+    }
+    else if([sender isEqualTo:_minWNilBtn]){
+        [self setValue:nil forCSSTag:IUCSSTagMinPixelWidth];
+        [self setValue:nil forCSSTag:IUCSSTagMinPixelWidth];
+    }
+    else if([sender isEqualTo:_minHNilBtn]){
+        [self setValue:nil forCSSTag:IUCSSTagMinPixelHeight];
+        [self setValue:nil forCSSTag:IUCSSTagMinPixelHeight];
+    }
+}
+
+
 - (void)dealloc{
-    NSArray *removeObservers = @[[self pathForCSSTag:IUCSSTagPixelX],
-                                 [self pathForCSSTag:IUCSSTagPixelY],
-                                 [self pathForCSSTag:IUCSSTagPixelWidth],
-                                 [self pathForCSSTag:IUCSSTagPixelHeight],
-                                 [self pathForCSSTag:IUCSSTagPercentX],
-                                 [self pathForCSSTag:IUCSSTagPercentY],
-                                 [self pathForCSSTag:IUCSSTagPercentWidth],
-                                 [self pathForCSSTag:IUCSSTagPercentHeight],
-                                 [self pathForCSSTag:IUCSSTagMinPixelHeight],
-                                 [self pathForCSSTag:IUCSSTagMinPixelWidth],
-                                 ];
-        
-    [self removeObserver:self forKeyPaths:removeObservers];
     [JDLogUtil log:IULogDealloc string:@"LMAppearanceFrameVC"];
 }
 
-#pragma mark - MQSize
-
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
-    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
-
-    if(enableObservation){
-        if ([[keyPath pathExtension] isSameTag:IUCSSTagPixelX]) {
-            [self setValueForTag:IUCSSTagPixelX toTextfield:_xTF toStepper:_xStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagPixelY]) {
-            [self setValueForTag:IUCSSTagPixelY toTextfield:_yTF toStepper:_yStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagPixelWidth]) {
-            [self setValueForTag:IUCSSTagPixelWidth toTextfield:_wTF toStepper:_wStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagPixelHeight]) {
-            [self setValueForTag:IUCSSTagPixelHeight toTextfield:_hTF toStepper:_hStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagPercentX]) {
-            [self setValueForTag:IUCSSTagPercentX toTextfield:_pxTF toStepper:_pxStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagPercentY]) {
-            [self setValueForTag:IUCSSTagPercentY toTextfield:_pyTF toStepper:_pyStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagPercentWidth]) {
-            [self setValueForTag:IUCSSTagPercentWidth toTextfield:_pwTF toStepper:_pwStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagPercentHeight]) {
-            [self setValueForTag:IUCSSTagPercentHeight toTextfield:_phTF toStepper:_phStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagMinPixelWidth]) {
-            [self setValueForTag:IUCSSTagMinPixelWidth toTextfield:_minWTF toStepper:_minWStepper];
-        }
-        else if ([[keyPath pathExtension] isSameTag:IUCSSTagMinPixelHeight]) {
-            [self setValueForTag:IUCSSTagMinPixelHeight toTextfield:_minHTF toStepper:_minHStepper];
-        }
-
-    }
-}
-
-
-
-- (void)setValueForTag:(IUCSSTag)tag toTextfield:(NSTextField*)textfield toStepper:(NSStepper *)stepper{
-    id value = [self valueForCSSTag:tag];
-   
-    [self setValue:value toStepper:stepper];
-    [self setValue:value toTextfield:textfield];
-    
-}
-
-- (void)setValue:(id)value toStepper:(NSStepper *)stepper{
-    if (value == nil || value == NSNoSelectionMarker || value == NSMultipleValuesMarker || value == NSNotApplicableMarker){
-        [stepper setEnabled:NO];
-    }
-    else{
-        [stepper setEnabled:YES];
-        [stepper setIntegerValue:[value integerValue]];
-    }
-}
-
-- (void)setValue:(id)value toTextfield:(NSTextField*)textfield{
-    //default setting
-    [[textfield cell] setPlaceholderString:@""];
-
-    if (value == nil || value == NSNoSelectionMarker || value == NSMultipleValuesMarker || value == NSNotApplicableMarker){
-        if(value){
-            //placehoder setting
-            NSString *placeholder = [NSString stringWithValueMarker:value];
-            [[textfield cell] setPlaceholderString:placeholder];
-            [textfield setStringValue:@""];
-            
-        }
-        else{
-            [textfield setStringValue:@"-"];
-        }
-    }
-    else{
-        if(value && [value isEqual:textfield.stringValue] == NO){
-            if([value isKindOfClass:[NSString class]]){
-                [textfield setStringValue:value];
-            }
-            else{
-                [textfield setStringValue:[value stringValue]];
-                if([value floatValue] < 0){
-                    [textfield setTextColor:[NSColor redColor]];
-                }
-                else{
-                    [textfield setTextColor:[NSColor blackColor]];
-                }
-            }
-        }
-    }
-}
-
-
-- (BOOL)control:(NSControl *)control textShouldEndEditing:(NSText *)fieldEditor{
-    enableObservation = NO;
-    IUCSSTag tag;
-    NSStepper *stepper;
-    if (control == _xTF) {
-        tag = IUCSSTagPixelX;
-        stepper = _xStepper;
-    }
-    else if (control == _pxTF) {
-        tag = IUCSSTagPercentX;
-        stepper = _pxStepper;
-    }
-    else if (control == _yTF) {
-        tag = IUCSSTagPixelY;
-        stepper = _yStepper;
-    }
-    else if (control == _pyTF) {
-        tag = IUCSSTagPercentY;
-        stepper = _pyStepper;
-    }
-    else if (control == _wTF) {
-        tag = IUCSSTagPixelWidth;
-        stepper = _wStepper;
-    }
-    else if (control == _pwTF) {
-        tag = IUCSSTagPercentWidth;
-        stepper = _pwStepper;
-    }
-    else if (control == _hTF) {
-        tag = IUCSSTagPixelHeight;
-        stepper = _hStepper;
-    }
-    else if (control == _phTF) {
-        tag = IUCSSTagPercentHeight;
-        stepper = _phStepper;
-    }
-    else if (control == _minWTF){
-        tag = IUCSSTagMinPixelWidth;
-        stepper = _minWStepper;
-    }
-    else if (control == _minHTF){
-        tag = IUCSSTagMinPixelHeight;
-        stepper = _minHStepper;
-    }
-    
-    [self setCSSFrameValue:[control stringValue] forTag:tag];
-    [self setValue:[control stringValue] toStepper:stepper];
-    enableObservation = YES;
-    return YES;
-}
-- (IBAction)clickStepper:(id)sender {
-    IUCSSTag tag;
-    enableObservation = NO;
-
-    NSTextField *textField;
-    if (sender == _xStepper) {
-        tag = IUCSSTagPixelX;
-        textField = _xTF;
-    }
-    else if (sender == _pxStepper) {
-        tag = IUCSSTagPercentX;
-        textField = _pxTF;
-    }
-    else if (sender == _yStepper) {
-        tag = IUCSSTagPixelY;
-        textField = _yTF;
-    }
-    else if (sender == _pyStepper) {
-        tag = IUCSSTagPercentY;
-        textField = _pyTF;
-    }
-    else if (sender == _wStepper) {
-        tag = IUCSSTagPixelWidth;
-        textField = _wTF;
-    }
-    else if (sender == _pwStepper) {
-        tag = IUCSSTagPercentWidth;
-        textField = _pwTF;
-    }
-    else if (sender == _hStepper) {
-        tag = IUCSSTagPixelHeight;
-        textField = _hTF;
-    }
-    else if (sender == _phStepper) {
-        tag = IUCSSTagPercentHeight;
-        textField = _phTF;
-    }
-    else if (sender == _minWStepper){
-        tag = IUCSSTagMinPixelWidth;
-        textField = _minWTF;
-    }
-    else if (sender == _minHStepper){
-        tag = IUCSSTagMinPixelHeight;
-        textField = _minHTF;
-    }
-    
-    [self setCSSFrameValue:[sender stringValue] forTag:tag];
-    [self setValue:[sender stringValue] toTextfield:textField];
-    enableObservation = YES;
-
-}
-
-- (void)setCSSFrameValue:(id)value forTag:(IUCSSTag)tag{
-    for (IUBox *iu in self.controller.selectedObjects) {
-        [iu startDragSession];
-    }
-
-    id updateValue = value;
-    if (value == nil || [value isEqualToString:@"-"]) {
-        updateValue = nil;
-    }
-    [self setValueWithouUpdateCSS:updateValue forCSSTag:tag];
-    for (IUBox *iu in self.controller.selectedObjects) {
-        [iu endDragSession];
-        [iu updateCSS];
-    }
-    
-}
 
 - (IBAction)helpMenu:(id)sender {
     JDTraceLog(@"this is help menu");
